fbcdbf12872ca82d53b4330e30278d4dc00c89ef
John Santos <jhow.mito11@gmail.com>
2022-03-31 11:02:18 -0300

    commit verificacao

---
+++ appGameplay/src/components/Category/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+
+export default StyleSheet.create ({
+container: {
+    height: 48,
+    width: 48,
+    backgroundColor: theme.colors.primary,
+   
+},
+
+content:{
+ 
+},
+
+checked:{
+
+},
+   
+check:{
+
+},
+
+title:{
+
+},
+
+})
